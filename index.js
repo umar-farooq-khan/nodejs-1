703fc1e8a58688ca0999bcb186b13e90487015a2
Umar Farooq khan <umaR.farooq407@gmail.com>
2024-07-22 14:24:01 +0100

    kkkk
    kkk

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello umarrr!');
+  res.end('Hello umarrr farooq!');
 });
 
 server.listen(PORT, () => {
