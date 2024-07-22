3c88e6cf321963aa73f9f676f99e4344f3de44d4
Umar Farooq khan <umaR.farooq407@gmail.com>
2024-07-22 15:34:26 +0100

    yyy

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const http = require('http');
-const PORT = 3000;
+const pdf = require('html-pdf');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello umarrr farooq!');
-});
+const htmlString = `
+  <html>
+    <head>
+      <title>Sample PDF</title>
+    </head>
+    <body>
+      <h1>Hello, World!</h1>
+      <p>This is a sample PDF created from an HTML string using html-pdf.</p>
+    </body>
+  </html>
+`;
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+pdf.create(htmlString).toFile('output.pdf', (err, res) => {
+  if (err) return console.log(err);
+  console.log(res); // { filename: '/path/to/output.pdf' }
 });
